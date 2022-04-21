784ac65f2f803c661dd434f2b40fa288b51c5149
jaeung <dlwodnd00@gmail.com>
2022-04-21 15:57:27 +0900

    Day 5

--- practice.py
+++ practice.py
@@ -1,2 +1,15 @@
-tmp_dict = {1:5, 2:4}
-print(tmp_dict[1])
+drinks = {
+    'martini': {'vodka', 'vermouth'},
+    'black russian': {'vodka','kahlua'},
+    'white russian': {'cream', 'kahlua', 'vodka'},
+    'manhattan': {'rye', 'vermouth', 'bitters'},
+    'screwdriver': {'orange juice', 'vodka'}
+    }
+for name, contents in drinks.items():
+        if {'vodka'} & contents and not contents & {'vermouth', 'orange juice'}:
+            print(name)
+            
+a = {1, 2}
+b = {2, 3}
+
+print(a & b, a.intersection(b), a | b, a.union(b))
