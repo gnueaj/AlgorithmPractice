eff78c0274da3907cf3c4aece5a35b6ec874c24d
jaeung <dlwodnd00@gmail.com>
2022-05-02 21:19:04 +0900

    Day 8

--- lunch.py
+++ lunch.py
@@ -1,11 +1,3 @@
-from logging import exception
-
-
-class UppercaseException(Exception):
-    pass # UppercaseException에 대한 행동 정의
-
-words = ['eenie', 'meenie', 'miny', 'MO']
-for word in words:
-    if word.isupper():
-        raise UppercaseException(word) # raise 로 예외 발생시키기
-    
+import sys
+for place in sys.path:
+    print(place)
