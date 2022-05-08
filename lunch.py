df23fa8f4527138ccddb64bb65c748cb71d0716d
jaeung <dlwodnd00@gmail.com>
2022-05-08 19:11:47 +0900

    test

--- lunch.py
+++ lunch.py
@@ -1,3 +1,7 @@
-import sys
-for place in sys.path:
-    print(place)
+def palindrome(word):
+    from collections import deque
+    dq = deque(word)
+    while len(dq) > 1:
+        if dq.popleft() != dq.pop():
+            return False
+    return True
