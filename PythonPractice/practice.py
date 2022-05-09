7ba038eca3c395f4bead5b7f32c4024ed1fa010c
jaeung <dlwodnd00@gmail.com>
2022-05-09 21:50:00 +0900

    내일 마무리

--- PythonPractice/practice.py
+++ PythonPractice/practice.py
@@ -1,14 +1,38 @@
-class Duck():
-    def __init__(self, input_name):
-        self.hidden_name = input_name
-    def get_name(self):
-        print('inside the getter')
-        return self.hidden_name
-    def set_name(self, input_name):
-        print('inside the setter')
-        self.hidden_name = input_name
+class A():
+    count = 0
+    def __init__(self):
+        A.count += 1
+    def exclaim(self):
+        print("I'm an A!")
+    @classmethod
+    def kids(cls):
+        print("A has", cls.count, "little objects.")
+
+easy_a = A()
+breezy_a = A()
+wheezy_a = A()
+A.kids()
+
+class CoyoteWeapon():
+    @staticmethod
+    def commercial():
+        print("This CoyoteWeapon has been brought to you by Acme")
         
-don = Duck('Donald')
-print(don.hidden_name)
-don.hidden_name = 'Donna'
-print(don.hidden_name)
+CoyoteWeapon.commercial()
+
+class Quote():
+    def __init__(self, person, words):
+        self.person = person
+        self.words = words
+    def who(self):
+        return self.person
+    def says(self):
+        return self.words + '.'
+
+class QuestionQuote(Quote)
+    def says(self):
+        return self.words + '?'
+    
+class ExclamationQuote(Quote):
+    def says(self):
+        return self.words + '!'
