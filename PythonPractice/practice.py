0c315b0f64d7b9f2241a494e5d82df2542a02269
jaeung <dlwodnd00@gmail.com>
2022-05-09 20:56:39 +0900

    에혀

--- PythonPractice/practice.py
+++ PythonPractice/practice.py
@@ -1,1 +1,14 @@
-ddddd
+class Duck():
+    def __init__(self, input_name):
+        self.hidden_name = input_name
+    def get_name(self):
+        print('inside the getter')
+        return self.hidden_name
+    def set_name(self, input_name):
+        print('inside the setter')
+        self.hidden_name = input_name
+        
+don = Duck('Donald')
+print(don.hidden_name)
+don.hidden_name = 'Donna'
+print(don.hidden_name)
