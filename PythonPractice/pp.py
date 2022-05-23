f2337d7579b1375152fba94422fc1f684ace4d89
jaeung <dlwodnd00@gmail.com>
2022-05-23 16:57:04 +0900

    end

--- PythonPractice/pp.py
+++ PythonPractice/pp.py
@@ -1,38 +1,8 @@
-class A():
-    count = 0
-    def __init__(self):
-        A.count += 1
-    def exclaim(self):
-        print("I'm an A!")
-    @classmethod
-    def kids(cls):
-        print("A has", cls.count, "little objects.")
-
-easy_a = A()
-breezy_a = A()
-wheezy_a = A()
-A.kids()
-
-class CoyoteWeapon():
-    @staticmethod
-    def commercial():
-        print("This CoyoteWeapon has been brought to you by Acme")
-        
-CoyoteWeapon.commercial()
-
-class Quote():
-    def __init__(self, person, words):
-        self.person = person
-        self.words = words
-    def who(self):
-        return self.person
-    def says(self):
-        return self.words + '.'
-
-class QuestionQuote(Quote)
-    def says(self):
-        return self.words + '?'
+from dataclasses import dataclass
+@dataclass
+class AnimalClass:
+    name: str
+    habitat: str
+    teeth: int = 0
     
-class ExclamationQuote(Quote):
-    def says(self):
-        return self.words + '!'
+snowman = AnimalClass('yeti', 'Himalayas', 46)
