a31daa4b011395939933b5dc984163abdf076e83
JaeUng <dlwodnd00@gmail.com>
2022-07-19 16:32:49 +0900

    ss22

---
+++ C++Algorithm/tmp.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <utility>
+#include <cstring>
+#include <map>
+#include <set>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+
+int genderToInt(char mGender[7])
+{
+    return strlen(mGender) != 4;
+}/* male -> 0, female -> 1 */
+
+int main()
+{
+}
